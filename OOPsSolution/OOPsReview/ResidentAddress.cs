e9dbe1d38490ff19dec042dfb1ef58aa407dbbb1
Don Welch <dwelch@nait.ca>
2024-09-18 17:19:07 -0600

    investigate the datatype record

---
+++ OOPsSolution/OOPsReview/ResidentAddress.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public record ResidentAddress(int Number, string Street, string City,
+                                    string Province, string PostalCode)
+    {
+        //this is a new datatype for .Net Core behaves very much like a class
+        //the fields are listed on the declaration
+        //this is a read only version of a class
+        //when declaring an instance you must pass in the data to be held
+        //your CANNOT alter the record instance
+        //if you need to alter the data within the instance you must
+        //      create a new instance and pass the data to the new instance
+
+        //OPTIONALLY!!!!
+        //you can add your own greedy constructor BUT the data values to be
+        //      store are the items on the record delcaration
+        //the constructor MUST have the same parameter list
+        //you can add methods to this datatype THAT DO NOT alter the data values
+        //these methods are returning a data value
+
+        public override string ToString()
+        {
+            return $"{Number},{Street},{City},{Province},{PostalCode}";
+        }
+    }
+}
