9da488af89591a3de8b6602cfe3407c696b21fc8
Don Welch <dwelch@nait.ca>
2024-11-15 17:50:49 -0700

    using navigational entity properties to display data from a secondary table (entity) - your primary/child scenarios

--- CSSolution/WestWindSystem/BLL/ShipmentServices.cs
+++ CSSolution/WestWindSystem/BLL/ShipmentServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,6 +27,27 @@ namespace WestWindSystem.BLL
 
         #region Services
 
+        // Used to navigate on the ShipmentQuery page to the Shipper talble
+        //  Technique A
+        //public List<Shipment> Shipment_GetByYearAndMonth(int year, int month)
+        //{
+        //    // for the query:
+        //    //      the context instance (DAL class instance)
+        //    //      the DbSet() property name (the dataset you wish from the database)
+        //    //      optionally the filter condition (.Where)
+        //    //      optionally order by (.OrderBy)
+        //    IEnumerable<Shipment> info = _context.Shipments
+        //                                        .Where(x => x.ShippedDate.Year == year
+        //                                                 && x.ShippedDate.Month == month)
+        //                                        .OrderBy(x => x.ShippedDate);
+        //    return info.ToList();
+        //}
+
+        //This uses the technique (b) discussed on the ShipmentTable page
+        //note there is a required using class, see Additional namespaces above.
+        //uses the .Include method to add navigational instances to the return record
+        //note the predicate uses the virtual navigational property of the Shipment entity
+        //This will include the associated record from the Shippers table (parent) for the shipment record (child)
         public List<Shipment> Shipment_GetByYearAndMonth(int year, int month)
         {
             // for the query:
@@ -34,6 +56,7 @@ namespace WestWindSystem.BLL
             //      optionally the filter condition (.Where)
             //      optionally order by (.OrderBy)
             IEnumerable<Shipment> info = _context.Shipments
+                                                .Include(x => x.ShipViaNavigation)
                                                 .Where(x => x.ShippedDate.Year == year
                                                          && x.ShippedDate.Month == month)
                                                 .OrderBy(x => x.ShippedDate);
